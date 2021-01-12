f19810bb1363b57a9ea666b9ecfc207ee6b2b89d
Joel Ring <joel.ring@gmail.com>
2021-01-12 12:04:27 +0100

    adds basic player controller

---
+++ Assets/Plugins/Scripts/SamplePlayerController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class SamplePlayerController : MonoBehaviour{
+    
+    public float playerSpeed = 0.1f;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"),0) * (playerSpeed * Time.deltaTime);
+    }
+}
