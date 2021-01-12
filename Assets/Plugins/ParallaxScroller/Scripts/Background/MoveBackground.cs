75c73c72333e6ebc21f3ea1a4b0acf9a13c75a73
Joel Ring <joel.ring@gmail.com>
2021-01-12 13:25:18 +0100

    adds backgroundhandler

---
+++ Assets/Plugins/ParallaxScroller/Scripts/Background/MoveBackground.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+namespace Background{
+    
+    [Serializable]
+    public class BackgroundSprite{
+        [SerializeField]private Sprite backgroundSprite;
+        [SerializeField]private bool flipSprite;
+        [SerializeField]private float cameraRelativeSpeed;
+    }
+    public class MoveBackground : MonoBehaviour{
+        [SerializeField] private BackgroundSprite[] backgroundSprites;
+        
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
