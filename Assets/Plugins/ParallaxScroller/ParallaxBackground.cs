14faf1300521f1379d477a6db92176e5c9552ea5
h-Nystrom <71410719+h-Nystrom@users.noreply.github.com>
2021-01-12 15:28:57 +0100

    Adds ParallaxBackground script

---
+++ Assets/Plugins/ParallaxScroller/ParallaxBackground.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class ParallaxBackground : MonoBehaviour{
+    [SerializeField] Vector2 parallaxEffectMultiplier;
+    [SerializeField] bool infiniteHorizontal;
+    [SerializeField] bool infiniteVertical;
+    Transform cameraTransform;
+    Vector3 lastCameraPosition;
+    float textureUnitSizeX;
+    float textureUnitSizeY;
+    
+    void Start(){
+        cameraTransform = Camera.main.transform;
+        lastCameraPosition = cameraTransform.position;
+        var sprite = GetComponent<SpriteRenderer>().sprite;
+        var temp = sprite.bounds.size.x;
+        
+        var texture2D = sprite.texture;
+        textureUnitSizeX = texture2D.width / sprite.pixelsPerUnit;
+    }
+    void LateUpdate(){
+        var deltaMovement = cameraTransform.position - lastCameraPosition;
+        transform.position += new Vector3(deltaMovement.x * parallaxEffectMultiplier.x, deltaMovement.y * parallaxEffectMultiplier.y,0);
+        lastCameraPosition = cameraTransform.position;
+        
+        if(Mathf.Abs(cameraTransform.position.x - transform.position.x) >= textureUnitSizeX){
+            var offsetPositionX = (cameraTransform.position.x - transform.position.x) % textureUnitSizeX;
+            transform.position = new Vector3(cameraTransform.position.x + offsetPositionX, transform.position.y,0);
+        }
+        if(Mathf.Abs(cameraTransform.position.y - transform.position.y) >= textureUnitSizeY){
+            var offsetPositionY = (cameraTransform.position.y - transform.position.y) % textureUnitSizeY;
+            transform.position = new Vector3(cameraTransform.position.x, transform.position.y + offsetPositionY,0);
+        }
+
+    }
+    ///Inspector:
+    ///Add layer:
+    ///Sprite, layer order, static, speed
+    ///Update button.
+    ///Sprite, layer order, static, speed
+    ///Update button.
+}
--- Assets/Plugins/ParallaxScroller/Scripts/Background/MoveBackground.cs
+++ Assets/Plugins/ParallaxScroller/Scripts/Background/MoveBackground.cs
@@ -3,32 +3,15 @@ using UnityEngine;
 
 namespace Background{
     
-    [Serializable]
-    public class BackgroundSprite{
-        [SerializeField]public Sprite backgroundSprite;
-        [SerializeField]public bool flipSprite;
-        [SerializeField]public float cameraRelativeSpeed;
-    }
     public class MoveBackground : MonoBehaviour{
-        [SerializeField] private BackgroundSprite[] backgroundSprites;
+        // [SerializeField] private BackgroundSprite[] backgroundSprites;
+        [SerializeField] GameObject prefab;
         
-        // Start is called before the first frame update
-        void Start()
-        {
-            foreach (var background in backgroundSprites){
-                //check if sprite need to be flipped
-            
-        
-                //check if background sprite needs to be cloned to fill entire screen with margin
-                int numberClones = Screen.width / background.backgroundSprite;
-                
-            }
-
-            
+        //TODO: Spawn gameobjects of sprites
+        //TODO: option to flip image
+        void Start(){
             
         }
-
-        // Update is called once per frame
         void Update()
         {
         
